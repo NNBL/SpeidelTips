ce1f19a26deb63c4d8e8a1611e2b0af1d69bfc9d
Henrik Width <henrik.width@gmail.com>
2014-02-18 14:31:43 +0100

    2nd order polinomial trend curve and error band

--- plot_all.py
+++ plot_all.py
@@ -4,38 +4,47 @@ import numpy as np
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
-weightdata = np.genfromtxt("weight_vs_efficiency.csv", \
-                        delimiter=",", names=True)
+weightdata = np.genfromtxt("weight_vs_efficiency.csv", delimiter=",", names=True)
 
-wfit = np.polyfit(weightdata["Grainweight_g"], \
-                        weightdata["Efficiency_"], 1)
-wfit_fn = np.poly1d(wfit)
+wx = weightdata["Grainweight_g"]
+wy = weightdata["Efficiency_"]
 
-attdata = np.genfromtxt("mash_temp_vs_attenuation.csv", \
-                        delimiter=",", names=True)
+#fit 2nd order polynomial
+wparams = np.polyfit(wx, wy, 2)
+wxp = np.linspace(7000, 2500, 20)
+wyp = np.polyval(wparams, wxp)
 
-afit = np.polyfit(attdata["Mash_temperature_C"], \
-                        attdata["Attenuation_"], 1)
-afit_fn = np.poly1d(afit)
+#error band
+wsig = np.std(wy - np.polyval(wparams, wx))
+
+attdata = np.genfromtxt("mash_temp_vs_attenuation.csv", delimiter=",", names=True)
+ax = attdata["Mash_temperature_C"]
+ay =  attdata["Attenuation_"]
+
+#fit 2nd order polynomial
+aparams = np.polyfit(ax, ay, 1)
+axp = np.linspace(80, 55, 2)
+ayp = np.polyval(aparams, axp)
+
+#error band
+asig = np.std(ay - np.polyval(aparams, ax))
 
 
 pl.subplot(2, 1, 1)
 pl.title("Braumeister efficiency")
 
-pl.plot(weightdata["Grainweight_g"], weightdata["Efficiency_"], \
-        "ro", weightdata["Grainweight_g"], \
-        wfit_fn(weightdata["Grainweight_g"]), "--g")
-
+pl.plot(weightdata["Grainweight_g"], weightdata["Efficiency_"], "ro")
+pl.plot(wxp, wyp, 'k')
+pl.fill_between(wxp, wyp - wsig, wyp + wsig, color='g', alpha=0.2)
 pl.xlabel("Grain weight [g]")
 pl.ylabel("Efficiency [%]")
 pl.grid()
 
 pl.subplot(2, 1, 2)
 pl.title("Mash temp vs yeast attenuation")
-
-pl.plot(attdata["Mash_temperature_C"], attdata["Attenuation_"], \
-        "bo", attdata["Mash_temperature_C"], \
-        afit_fn(attdata["Mash_temperature_C"]), "--g")
+pl.plot(attdata["Mash_temperature_C"], attdata["Attenuation_"], "ro")
+pl.plot(axp, ayp, 'k')
+pl.fill_between(axp, ayp - asig, ayp + asig, color='g', alpha=0.2)
 
 pl.xlabel("Temperature [C]")
 pl.ylabel("Attenuation [%]")
