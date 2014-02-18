48bb55a0720d3e6996b09a20506402aad50ab8b1
Henrik Width <henrik.width@gmail.com>
2014-02-18 12:59:10 +0100

    First stab at showing both csv files in same window

--- plot_weight_vs_efficiency.py
+++ plot_weight_vs_efficiency.py
@@ -1,11 +1,22 @@
 import matplotlib.pyplot as pl
 import numpy as np
 
-data = np.genfromtxt("weight_vs_efficiency.csv",delimiter=",", names=True)
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
 
-pl.scatter(data["Grainweight_g"],data["Efficiency_"])
+weightdata = np.genfromtxt("weight_vs_efficiency.csv",delimiter=",", names=True)
+attdata = np.genfromtxt("mash_temp_vs_attenuation.csv",delimiter=",", names=True)
+
+pl.subplot(2,1,1)
+pl.title("Braumeister efficiency")
+pl.scatter(weightdata["Grainweight_g"],weightdata["Efficiency_"])
 pl.xlabel("Grain weight [g]")
 pl.ylabel("Efficiency [%]")
 pl.grid()
-pl.title("20L Braumeister efficiency, NNBL 2014")
+pl.subplot(2,1,2)
+pl.title("Mash temp vs yeast attenuation")
+pl.scatter(attdata["Mash_temperature_C"],attdata["Attenuation_"], color="red")
+pl.xlabel("Temperature [C]")
+pl.ylabel("Attenuation [%]")
+pl.grid()
 pl.show()
