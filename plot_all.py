b1b1c26b1159b3294c2ba83ab4b6459897474b90
Henrik Width <henrik.width@gmail.com>
2014-02-20 11:02:27 +0100

    plotting different yeast types now has different colors
    legend also added.

--- plot_all.py
+++ plot_all.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as pl
 import numpy as np
+import pandas
+import matplotlib.cm as cm
 
 from matplotlib import rcParams
 rcParams.update({'figure.autolayout': True})
 
-weightdata = np.genfromtxt("weight_vs_efficiency.csv", delimiter=",", names=True)
+weightdata = np.genfromtxt("weight_vs_efficiency.csv", delimiter=",", names=True, dtype=None)
 
 wx = weightdata["Grainweight_g"]
 wy = weightdata["Efficiency_"]
@@ -16,14 +18,16 @@ wyp = np.polyval(wparams, wxp)
 
 #error band
 wsig = np.std(wy - np.polyval(wparams, wx))
-
-attdata = np.genfromtxt("mash_temp_vs_attenuation.csv", delimiter=",", names=True)
+attdata = np.genfromtxt("mash_temp_vs_attenuation.csv", delimiter=",", names=True, dtype=None)
+##Experimental and ugly
+data = pandas.read_csv("mash_temp_vs_attenuation.csv", delimiter=",", dtype=None)
+yeasttypes = set(data["Yeast type"])
+## End
 ax = attdata["Mash_temperature_C"]
 ay =  attdata["Attenuation_"]
 
 #fit 2nd order polynomial
-aparams = np.polyfit(ax, ay,
- 2)
+aparams = np.polyfit(ax, ay, 2)
 axp = np.linspace(80, 60, 100)
 ayp = np.polyval(aparams, axp)
 
@@ -44,15 +48,30 @@ pl.grid()
 
 pl.subplot(2, 1, 2)
 pl.title("Mash temp vs yeast attenuation", fontsize=16)
-
 pl.plot(axp, ayp, 'k')
 pl.fill_between(axp, ayp - asig, ayp + asig, color='green', alpha=0.25)
-pl.plot(attdata["Mash_temperature_C"], attdata["Attenuation_"], "ro")
+#Experimental and ugly
+
+Ex = np.arange(len(yeasttypes))
+Eys = [i+Ex+(i*Ex)**2 for i in range(len(yeasttypes))]
+
+colors = cm.rainbow(np.linspace(0, 1, len(Eys)))
+
+#colors = ["red","blue","green","orange","purple"]
+
+
+for yeast,color in zip(yeasttypes,colors):
+	brews = data[data["Yeast type"].str.contains(yeast)]
+	pl.scatter(brews["Mash temperature [C]"], brews["Attenuation [%]"], color=color, edgecolors="black", label=yeast)
+#End
+
+
 pl.xlim(60,80)
 pl.ylim(40,100)
 pl.xlabel(r"Temperature [$^{\circ}\mathrm{C}$]")
 pl.ylabel("Attenuation [%]")
 pl.grid()
+pl.legend()
 pl.tight_layout()
 pl.savefig('all.png')
 pl.show()
