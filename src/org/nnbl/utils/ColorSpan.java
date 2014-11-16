7d9a26aa9f43ac7a75e226942702899f57a7521a
Henrik Width <henrik.width@gmail.com>
2014-11-16 12:48:27 +0100

    added gray and white to the color span

--- src/org/nnbl/utils/ColorSpan.java
+++ src/org/nnbl/utils/ColorSpan.java
@@ -5,10 +5,15 @@ import java.awt.Color;
 public class ColorSpan {
 	public static Color[] generateColorSpan(int numberOfColors) {
 		Color[] colors = new Color[numberOfColors];
-		for (int i = 0; i < numberOfColors; i++) {
+		
+		for (int i = 0; i < numberOfColors - 2; i++) {
 			colors[i] = Color
 					.getHSBColor((1f / numberOfColors) * i, 1.0f, 1.0f);
 		}
+
+		colors[numberOfColors - 2] = Color.GRAY;
+		colors[numberOfColors - 1] = Color.WHITE;
+
 		return colors;
 	}
 
@@ -31,8 +36,8 @@ public class ColorSpan {
 			double r = Math.sin(frequency1 * i + phase1) * width + center;
 			double g = Math.sin(frequency2 * i + phase2) * width + center;
 			double b = Math.sin(frequency3 * i + phase3) * width + center;
-			colors[i] = new Color((int)r, (int)g, (int)b);
-			System.out.println("r:"+r+", g:"+g+", b:"+b);
+			colors[i] = new Color((int) r, (int) g, (int) b);
+			System.out.println("r:" + r + ", g:" + g + ", b:" + b);
 		}
 		return colors;
 	}
