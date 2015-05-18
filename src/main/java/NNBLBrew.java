d71e7317aa084ec1b2c5efff1921989cb69b559f
Henrik Width <henrik.width@gmail.com>
2015-05-19 00:24:15 +0200

    google style formatting

--- src/main/java/NNBLBrew.java
+++ src/main/java/NNBLBrew.java
@@ -3,137 +3,136 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class NNBLBrew {
-	private SimpleStringProperty brewer;
-	private SimpleStringProperty brewname;
-	private SimpleIntegerProperty grainweight;
-	private SimpleDoubleProperty efficiency;
-	private SimpleIntegerProperty originalGravity;
-	private SimpleIntegerProperty finalGravity;
-	private SimpleDoubleProperty mashTemp;
-	private SimpleDoubleProperty attenuation;
-	private SimpleStringProperty yeasttype;
-	private SimpleStringProperty remarks;
+  private SimpleStringProperty brewer;
+  private SimpleStringProperty brewname;
+  private SimpleIntegerProperty grainweight;
+  private SimpleDoubleProperty efficiency;
+  private SimpleIntegerProperty originalGravity;
+  private SimpleIntegerProperty finalGravity;
+  private SimpleDoubleProperty mashTemp;
+  private SimpleDoubleProperty attenuation;
+  private SimpleStringProperty yeasttype;
+  private SimpleStringProperty remarks;
 
 
-	public NNBLBrew(String[] values) {
-		this.brewer = new SimpleStringProperty(values[0]);
-		this.brewname = new SimpleStringProperty(values[1]);
-		this.grainweight = new SimpleIntegerProperty(Integer.parseInt(values[2].trim()));
-		this.efficiency = new SimpleDoubleProperty(Double.parseDouble(values[3].trim()));
+  public NNBLBrew(String[] values) {
+    this.brewer = new SimpleStringProperty(values[0]);
+    this.brewname = new SimpleStringProperty(values[1]);
+    this.grainweight = new SimpleIntegerProperty(Integer.parseInt(values[2].trim()));
+    this.efficiency = new SimpleDoubleProperty(Double.parseDouble(values[3].trim()));
 
-		if (values[4].equals("NaN")) {
-			this.originalGravity = new SimpleIntegerProperty(-1);
-		} else {
-			this.originalGravity = new SimpleIntegerProperty(Integer.parseInt(values[4].trim()));
-		}
+    if (values[4].equals("NaN")) {
+      this.originalGravity = new SimpleIntegerProperty(-1);
+    } else {
+      this.originalGravity = new SimpleIntegerProperty(Integer.parseInt(values[4].trim()));
+    }
 
-		if (values[4].equals("NaN")) {
-			this.finalGravity = new SimpleIntegerProperty(-1);
+    if (values[4].equals("NaN")) {
+      this.finalGravity = new SimpleIntegerProperty(-1);
 
-		} else {
-			this.finalGravity = new SimpleIntegerProperty(Integer.parseInt(values[5].trim()));
-		}
+    } else {
+      this.finalGravity = new SimpleIntegerProperty(Integer.parseInt(values[5].trim()));
+    }
 
-		this.mashTemp = new SimpleDoubleProperty(Double.parseDouble(values[6].trim()));
-		this.attenuation = new SimpleDoubleProperty(Double.parseDouble(values[7].trim()));
-		this.yeasttype =new SimpleStringProperty( values[8].trim());
-		this.remarks = new SimpleStringProperty(values[9].trim());
-	}
+    this.mashTemp = new SimpleDoubleProperty(Double.parseDouble(values[6].trim()));
+    this.attenuation = new SimpleDoubleProperty(Double.parseDouble(values[7].trim()));
+    this.yeasttype = new SimpleStringProperty(values[8].trim());
+    this.remarks = new SimpleStringProperty(values[9].trim());
+  }
 
-	public String getBrewer() {
-		return brewer.get();
-	}
+  public String getBrewer() {
+    return brewer.get();
+  }
 
-	public void setBrewer(String brewer) {
-		this.brewer.set(brewer);
-	}
+  public void setBrewer(String brewer) {
+    this.brewer.set(brewer);
+  }
 
-	public String getBrewname() {
-		return brewname.get();
-	}
+  public String getBrewname() {
+    return brewname.get();
+  }
 
-	public void setBrewname(String brewname) {
-		this.brewname.set(brewname);
-	}
+  public void setBrewname(String brewname) {
+    this.brewname.set(brewname);
+  }
 
-	public int getGrainweight() {
-		return grainweight.get();
-	}
+  public int getGrainweight() {
+    return grainweight.get();
+  }
 
-	public void setGrainweight(int grainweight) {
-		this.grainweight.set(grainweight);
-	}
+  public void setGrainweight(int grainweight) {
+    this.grainweight.set(grainweight);
+  }
 
-	public double getEfficiency() {
-		return efficiency.get();
-	}
+  public double getEfficiency() {
+    return efficiency.get();
+  }
 
-	public void setEfficiency(double efficiency) {
-		this.efficiency.set(efficiency);
-	}
+  public void setEfficiency(double efficiency) {
+    this.efficiency.set(efficiency);
+  }
 
-	public int getOriginalGravity() {
-		return originalGravity.get();
-	}
+  public int getOriginalGravity() {
+    return originalGravity.get();
+  }
 
-	public void setOriginalGravity(int originalGravity) {
-		this.originalGravity.set(originalGravity);
-	}
+  public void setOriginalGravity(int originalGravity) {
+    this.originalGravity.set(originalGravity);
+  }
 
-	public int getFinalGravity() {
-		return finalGravity.get();
-	}
+  public int getFinalGravity() {
+    return finalGravity.get();
+  }
 
-	public void setFinalGravity(int finalGravity) {
-		this.finalGravity.set(finalGravity);
-	}
+  public void setFinalGravity(int finalGravity) {
+    this.finalGravity.set(finalGravity);
+  }
 
-	public double getMashTemp() {
-		return mashTemp.get();
-	}
+  public double getMashTemp() {
+    return mashTemp.get();
+  }
 
-	public void setMashTemp(double mashTemp) {
-		this.mashTemp.set(mashTemp);
-	}
+  public void setMashTemp(double mashTemp) {
+    this.mashTemp.set(mashTemp);
+  }
 
-	public double getAttenuation() {
-		return attenuation.get();
-	}
+  public double getAttenuation() {
+    return attenuation.get();
+  }
 
-	public void setAttenuation(double attenuation) {
-		this.attenuation.set(attenuation);
-	}
+  public void setAttenuation(double attenuation) {
+    this.attenuation.set(attenuation);
+  }
 
-	public String getYeasttype() {
-		return yeasttype.get();
-	}
+  public String getYeasttype() {
+    return yeasttype.get();
+  }
 
-	public void setYeasttype(String yeasttype) {
-		this.yeasttype.set(yeasttype);
-	}
+  public void setYeasttype(String yeasttype) {
+    this.yeasttype.set(yeasttype);
+  }
 
-	public String getRemarks() {
-		return remarks.get();
-	}
+  public String getRemarks() {
+    return remarks.get();
+  }
 
-	public void setRemarks(String remarks) {
-		this.remarks.set(remarks);
-	}
+  public void setRemarks(String remarks) {
+    this.remarks.set(remarks);
+  }
 
 
-	@Override
-	public String toString() {
+  @Override public String toString() {
 
-		String s = brewname.get() + " by " + brewer.get() + System.lineSeparator();
-		s += "Grain weight: " + grainweight.get() + "g" + System.lineSeparator();
-		s += "Efficiency: " + efficiency.get() + "%" + System.lineSeparator();
-		s += "OG: " + originalGravity.get() + System.lineSeparator();
-		s += "FG: " + finalGravity.get() + System.lineSeparator();
-		s += "Mash temp: " + mashTemp.get() + "C" + System.lineSeparator();
-		s += "Attanuation: " + attenuation.get() + System.lineSeparator();
-		s += "Yeast type: " + yeasttype.get() + System.lineSeparator();
-		s += "Remarks: " + remarks.get() + System.lineSeparator();
+    String s = brewname.get() + " by " + brewer.get() + System.lineSeparator();
+    s += "Grain weight: " + grainweight.get() + "g" + System.lineSeparator();
+    s += "Efficiency: " + efficiency.get() + "%" + System.lineSeparator();
+    s += "OG: " + originalGravity.get() + System.lineSeparator();
+    s += "FG: " + finalGravity.get() + System.lineSeparator();
+    s += "Mash temp: " + mashTemp.get() + "C" + System.lineSeparator();
+    s += "Attanuation: " + attenuation.get() + System.lineSeparator();
+    s += "Yeast type: " + yeasttype.get() + System.lineSeparator();
+    s += "Remarks: " + remarks.get() + System.lineSeparator();
 
-		return s;
-	}
+    return s;
+  }
 }
--- src/main/java/NNBLBrewDatabase.java
+++ src/main/java/NNBLBrewDatabase.java
@@ -1,210 +1,212 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class NNBLBrewDatabase {
-	public static final int EFFICIENCY = 0;
-	public static final int GRAIN_WEIGHT = 1;
-	public static final int ORIGINAL_GRAVITY = 2;
-	public static final int FINAL_GRAVITY = 3;
-	public static final int ATTENUATION = 4;
-	public static final int MASH_TEMPERATURE = 5;
+  public static final int EFFICIENCY = 0;
+  public static final int GRAIN_WEIGHT = 1;
+  public static final int ORIGINAL_GRAVITY = 2;
+  public static final int FINAL_GRAVITY = 3;
+  public static final int ATTENUATION = 4;
+  public static final int MASH_TEMPERATURE = 5;
 
-	private ArrayList<NNBLBrew> db;
+  private ArrayList<NNBLBrew> db;
 
-	public NNBLBrewDatabase() {
-		db = new ArrayList<NNBLBrew>();
-	}
+  public NNBLBrewDatabase() {
+    db = new ArrayList<NNBLBrew>();
+  }
 
-	public ArrayList<Double> getAllEfficiency(boolean addIfZeroes) {
-		ArrayList<Double> values = new ArrayList<Double>();
-		for (NNBLBrew brew : db) {
-			if (addIfZeroes) {
-				values.add(brew.getEfficiency());
-			} else {
-				if (brew.getEfficiency() > 0) {
-					values.add(brew.getEfficiency());
-				}
-			}
-		}
-		return values;
-	}
-
-	public ArrayList<Double> getAllGrainWeight(boolean addIfZeroes) {
-		ArrayList<Double> values = new ArrayList<Double>();
-		for (NNBLBrew brew : db) {
-			if (addIfZeroes) {
-				values.add((double) brew.getGrainweight());
-			} else {
-				if (brew.getGrainweight() > 0) {
-					values.add((double) brew.getGrainweight());
-				}
-			}
-		}
-		return values;
-	}
-
-	public ArrayList<Double> getAllOG(boolean addIfZeroes) {
-		ArrayList<Double> values = new ArrayList<Double>();
-		for (NNBLBrew brew : db) {
-			if (addIfZeroes) {
-				values.add((double) brew.getOriginalGravity());
-
-			} else {
-				if (brew.getOriginalGravity() > 0) {
-					values.add((double) brew.getOriginalGravity());
-				}
-			}
-		}
-		return values;
-	}
-
-	public ArrayList<Double> getAllFG(boolean addIfZeroes) {
-		ArrayList<Double> values = new ArrayList<Double>();
-		for (NNBLBrew brew : db) {
-			if (addIfZeroes) {
-				values.add((double) brew.getFinalGravity());
-
-			} else {
-				if (brew.getFinalGravity() > 0) {
-					values.add((double) brew.getFinalGravity());
-				}
-			}
-		}
-		return values;
-	}
-
-	public ArrayList<Double> getAllAttenuation(boolean addIfZeroes) {
-		ArrayList<Double> values = new ArrayList<Double>();
-		for (NNBLBrew brew : db) {
-			if (addIfZeroes) {
-				values.add(brew.getAttenuation());
-
-			} else {
-				if (brew.getAttenuation() > 0) {
-					values.add(brew.getAttenuation());
-				}
-			}
-		}
-		return values;
-	}
-
-	public ArrayList<Double> getAllMashTemp(boolean addIfZeroes) {
-		ArrayList<Double> values = new ArrayList<Double>();
-		for (NNBLBrew brew : db) {
-			if (addIfZeroes) {
-				values.add(brew.getMashTemp());
-
-			} else {
-				if (brew.getMashTemp() > 0) {
-					values.add(brew.getMashTemp());
-				}
-			}
-		}
-		return values;
-	}
-
-	public void addBrew(NNBLBrew brew) {
-		db.add(brew);
-	}
-
-	public ArrayList<NNBLBrew> getBrews() {
-		return db;
-	}
-
-	public int size() {
-		return db.size();
-	}
-
-	public String findNearestEfficiency(float grainweight, float efficiency) {
-
-		StringBuilder nearest = new StringBuilder();
-
-		for (NNBLBrew brew : db) {
-			if (brew.getGrainweight() >= (grainweight - 50)
-					&& brew.getGrainweight() <= (grainweight + 50)
-					&& brew.getEfficiency() >= (efficiency - 0.4)
-					&& brew.getEfficiency() <= (efficiency + 0.4)) {
-				nearest.append(brew.getBrewname() + " by " + brew.getBrewer() + ", ");
-			}
-		}
-		return nearest.toString();
-	}
-
-	public String findNearestMashTemp(float temperature, float attenuation) {
-
-		StringBuilder nearest = new StringBuilder();
-
-		for (NNBLBrew brew : db) {
-			if (brew.getMashTemp() >= (temperature - 0.5)
-					&& brew.getMashTemp() <= (temperature + 0.5)
-					&& brew.getAttenuation() >= (attenuation - 0.4)
-					&& brew.getAttenuation() <= (attenuation + 0.4)) {
-				nearest.append(brew.getYeasttype() + " in " + brew.getBrewname() + ", ");
-			}
-		}
-		return nearest.toString();
-	}
-
-	public int getNumberOfBrewsWith(String yeastType) {
-		int number = 0;
-		for (NNBLBrew brew : db) {
-			if (brew.getYeasttype().equals(yeastType)) {
-				number++;
-			}
-		}
-		return number;
-	}
-
-    public String dbAsCsv(){
-        StringBuilder csv = new StringBuilder("Brewer,Name,Grainweight,Efficiency,OG,FG,Mash temperature,Attenuation,Yeast type,Remarks\n");
-
-        for (NNBLBrew nnblBrew : db) {
-            csv.append(nnblBrew.getBrewer());
-            csv.append(',');
-            csv.append(nnblBrew.getBrewname());
-            csv.append(',');
-            csv.append(nnblBrew.getGrainweight());
-            csv.append(',');
-            csv.append(nnblBrew.getEfficiency());
-            csv.append(',');
-            csv.append(nnblBrew.getOriginalGravity());
-            csv.append(',');
-            csv.append(nnblBrew.getFinalGravity());
-            csv.append(',');
-            csv.append(nnblBrew.getMashTemp());
-            csv.append(',');
-            csv.append(nnblBrew.getAttenuation());
-            csv.append(',');
-            csv.append(nnblBrew.getYeasttype());
-            csv.append(',');
-            csv.append(nnblBrew.getRemarks());
-            csv.append('\n');
-
+  public ArrayList<Double> getAllEfficiency(boolean addIfZeroes) {
+    ArrayList<Double> values = new ArrayList<Double>();
+    for (NNBLBrew brew : db) {
+      if (addIfZeroes) {
+        values.add(brew.getEfficiency());
+      } else {
+        if (brew.getEfficiency() > 0) {
+          values.add(brew.getEfficiency());
         }
-        return csv.toString();
+      }
     }
+    return values;
+  }
 
-	public void loadBrewsFromFile(String pathname) {
-		db.clear();
-		try {
-			File f = new File(pathname);
-			if (f.exists()) {
-				BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(pathname), "UTF8"));
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					if (!line.startsWith("Brewer,Name")) {
-						String[] values = line.split(",");
-						db.add(new NNBLBrew(values));
-					}
-				}
-				reader.close();
-			} else {
-				System.err.println("File " + pathname + " does not exist!\n");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+  public ArrayList<Double> getAllGrainWeight(boolean addIfZeroes) {
+    ArrayList<Double> values = new ArrayList<Double>();
+    for (NNBLBrew brew : db) {
+      if (addIfZeroes) {
+        values.add((double) brew.getGrainweight());
+      } else {
+        if (brew.getGrainweight() > 0) {
+          values.add((double) brew.getGrainweight());
+        }
+      }
+    }
+    return values;
+  }
+
+  public ArrayList<Double> getAllOG(boolean addIfZeroes) {
+    ArrayList<Double> values = new ArrayList<Double>();
+    for (NNBLBrew brew : db) {
+      if (addIfZeroes) {
+        values.add((double) brew.getOriginalGravity());
+
+      } else {
+        if (brew.getOriginalGravity() > 0) {
+          values.add((double) brew.getOriginalGravity());
+        }
+      }
+    }
+    return values;
+  }
+
+  public ArrayList<Double> getAllFG(boolean addIfZeroes) {
+    ArrayList<Double> values = new ArrayList<Double>();
+    for (NNBLBrew brew : db) {
+      if (addIfZeroes) {
+        values.add((double) brew.getFinalGravity());
+
+      } else {
+        if (brew.getFinalGravity() > 0) {
+          values.add((double) brew.getFinalGravity());
+        }
+      }
+    }
+    return values;
+  }
+
+  public ArrayList<Double> getAllAttenuation(boolean addIfZeroes) {
+    ArrayList<Double> values = new ArrayList<Double>();
+    for (NNBLBrew brew : db) {
+      if (addIfZeroes) {
+        values.add(brew.getAttenuation());
+
+      } else {
+        if (brew.getAttenuation() > 0) {
+          values.add(brew.getAttenuation());
+        }
+      }
+    }
+    return values;
+  }
+
+  public ArrayList<Double> getAllMashTemp(boolean addIfZeroes) {
+    ArrayList<Double> values = new ArrayList<Double>();
+    for (NNBLBrew brew : db) {
+      if (addIfZeroes) {
+        values.add(brew.getMashTemp());
+
+      } else {
+        if (brew.getMashTemp() > 0) {
+          values.add(brew.getMashTemp());
+        }
+      }
+    }
+    return values;
+  }
+
+  public void addBrew(NNBLBrew brew) {
+    db.add(brew);
+  }
+
+  public ArrayList<NNBLBrew> getBrews() {
+    return db;
+  }
+
+  public int size() {
+    return db.size();
+  }
+
+  public String findNearestEfficiency(float grainweight, float efficiency) {
+
+    StringBuilder nearest = new StringBuilder();
+
+    for (NNBLBrew brew : db) {
+      if (brew.getGrainweight() >= (grainweight - 50) && brew.getGrainweight() <= (grainweight + 50)
+        && brew.getEfficiency() >= (efficiency - 0.4) && brew.getEfficiency() <= (efficiency
+        + 0.4)) {
+        nearest.append(brew.getBrewname() + " by " + brew.getBrewer() + ", ");
+      }
+    }
+    return nearest.toString();
+  }
+
+  public String findNearestMashTemp(float temperature, float attenuation) {
+
+    StringBuilder nearest = new StringBuilder();
+
+    for (NNBLBrew brew : db) {
+      if (brew.getMashTemp() >= (temperature - 0.5) && brew.getMashTemp() <= (temperature + 0.5)
+        && brew.getAttenuation() >= (attenuation - 0.4) && brew.getAttenuation() <= (attenuation
+        + 0.4)) {
+        nearest.append(brew.getYeasttype() + " in " + brew.getBrewname() + ", ");
+      }
+    }
+    return nearest.toString();
+  }
+
+  public int getNumberOfBrewsWith(String yeastType) {
+    int number = 0;
+    for (NNBLBrew brew : db) {
+      if (brew.getYeasttype().equals(yeastType)) {
+        number++;
+      }
+    }
+    return number;
+  }
+
+  public String dbAsCsv() {
+    StringBuilder csv = new StringBuilder(
+      "Brewer,Name,Grainweight,Efficiency,OG,FG,Mash temperature,Attenuation,Yeast type,Remarks\n");
+
+    for (NNBLBrew nnblBrew : db) {
+      csv.append(nnblBrew.getBrewer());
+      csv.append(',');
+      csv.append(nnblBrew.getBrewname());
+      csv.append(',');
+      csv.append(nnblBrew.getGrainweight());
+      csv.append(',');
+      csv.append(nnblBrew.getEfficiency());
+      csv.append(',');
+      csv.append(nnblBrew.getOriginalGravity());
+      csv.append(',');
+      csv.append(nnblBrew.getFinalGravity());
+      csv.append(',');
+      csv.append(nnblBrew.getMashTemp());
+      csv.append(',');
+      csv.append(nnblBrew.getAttenuation());
+      csv.append(',');
+      csv.append(nnblBrew.getYeasttype());
+      csv.append(',');
+      csv.append(nnblBrew.getRemarks());
+      csv.append('\n');
+
+    }
+    return csv.toString();
+  }
+
+  public void loadBrewsFromFile(String pathname) {
+    db.clear();
+    try {
+      File f = new File(pathname);
+      if (f.exists()) {
+        BufferedReader reader =
+          new BufferedReader(new InputStreamReader(new FileInputStream(pathname), "UTF8"));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+          if (!line.startsWith("Brewer,Name")) {
+            String[] values = line.split(",");
+            db.add(new NNBLBrew(values));
+          }
+        }
+        reader.close();
+      } else {
+        System.err.println("File " + pathname + " does not exist!\n");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
